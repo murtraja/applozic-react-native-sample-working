a4b057b13bbe4c40aa6c9db5e4c4fc5ea981d720
Murtaza Raja <murtraja@gmail.com>
2019-02-28 12:19:40 +0530

    Sample working

--- ApplozicSample/android/app/src/main/java/com/applozicsample/MainApplication.java
+++ ApplozicSample/android/app/src/main/java/com/applozicsample/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.applozic.ApplozicChatPackage;
 import com.facebook.react.ReactApplication;
+// import com.reactlibrary.ApplozicChatPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(), new ApplozicChatPackage()
+          new MainReactPackage(),
+            new ApplozicChatPackage()
       );
     }
 
